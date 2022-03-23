d406e6cab0f08a92e6e6bbcbfab210e6852706fd
Bienfait-ijambo <ijamboizuba20@gmail.com>
2022-03-23 13:23:45 +0100

    search functionnalty is not working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   let userList = document.querySelector("#result");
-  let searchVal = document.querySelector("#filter");
+  let searchInput = document.querySelector("#filter");
   let html = "";
+  let filteredName = [];
 
-  let apiUser = [
+  let userName = [
     {
       name: "Bienfait ijambo",
       bio: "SoftWare ingeneer",
@@ -21,30 +22,46 @@ document.addEventListener("DOMContentLoaded", function () {
       bio: "SoftWare ingeneer",
     },
     {
-        name: "Bisimwa",
-        bio: "SoftWare ingeneer",
-      },
+      name: "Xavier",
+      bio: "SoftWare ingeneer",
+    },
   ];
 
-  function displayUser(searchVal = "") {
-    apiUser.forEach((user, index) => {
+  if (searchInput.value === "") {
+    displayUserName(userName);
+  }
+
+  searchInput.addEventListener("keyup", (e) => {
+    userName.filter((user, index) => {
+      if (user.name.includes(e.target.value)) {
+        const bool = filteredName.some((el) => el.name == user.name);
+        if (!bool) {
+          filteredName.push(userName[index]);
+        }
+      }
+      // document.querySelector("#result").innerHTML=""
+      // displayUserName(filteredName);
+      // console.log(filteredName);
+    });
+  });
+
+  // function findElementInArray(array){
+  //   const bool=array.some((el)=>el.name=array.name)
+
+  // }
+
+  function displayUserName(userArray) {
+    // userList.innerHTML="";
+    setTimeout(() => (userList.innerHTML = ""), 2000);
+    userArray.forEach((user) => {
       html += ` <li>
             <img  src="./img/avatar.png" alt="">
             <div class="user-info">
                 <h4>${user.name}</h4>
                 <p>${user.bio}</p>
             </div>
-            </li>`;
-
+         </li>`;
       userList.innerHTML = html;
-      //   console.log(user)
     });
   }
-
-  displayUser();
-
-  searchVal.addEventListener("keyup", (e) => {
-    displayUser();
-  });
-
 });
